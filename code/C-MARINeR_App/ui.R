6da80f2ef10d93b1e811680d63dd5aa689973406
jennyrieck <jenny.rieck@gmail.com>
2019-04-24 13:59:26 -0400

    bare bones shiny app

--- code/C-MARINeR_App/ui.R
+++ code/C-MARINeR_App/ui.R
@@ -1,33 +1,44 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
+shinyUI(
   
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
+  fluidPage(
     
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+    
+    # Application title
+    titlePanel("c-MARINeR"),
+    
+    sidebarLayout(
+      sidebarPanel(
+        h2('Analysis Options'),
+        selectInput('cubeIn', 'Select Connectivity Cube', 
+                    choices=arrayList),
+        actionButton('go', 'Run Analysis'),
+        h2('Plotting Options'),
+        selectInput('rowColDesign', 'Select Row/Column Design', 
+                    choices=designList),
+        numericInput('fp_xaxis', 'X Axis Component #', value=1,min=1,max=10,
+                     step=1),
+        numericInput('fp_yaxis', 'Y Axis Component #', value=2,min=1,max=10,
+                     step=1),
+        sliderInput('heatmap_lvs', 'Rebuilt HeatMap Components', min=1,max=10,
+                    value=c(1,2)),
+        checkboxInput('fp_labels', 'ROI Labels', value=F),
+        actionButton('replot', 'Plot Results'),
+        br(),
+        plotOutput('scree1'),
+        plotOutput('scree2')
+        #actionbutton('run','Run Analysis')
+      ),
+      
+      mainPanel(plotOutput('compromise_factor_map'),
+                br(),
+                plotOutput('compromise_heat_map')
+                
+      )
+      
     )
   )
-))
+)
+
