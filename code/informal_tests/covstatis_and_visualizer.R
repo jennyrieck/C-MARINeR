6369eb393a0ac0f073932fc42158990591eb38f8
Derek <derekbeaton@users.noreply.github.com>
2019-05-21 12:19:47 -0400

    slight updates to show the overall steps

--- code/informal_tests/covstatis_and_visualizer.R
+++ code/informal_tests/covstatis_and_visualizer.R
@@ -13,6 +13,14 @@ cov_matrices <- list(images=-as.matrix(jocn.2005.fmri$images$data),
 
 
 covstatis_results <- covstatis(cov_matrices)
+## covstatis has multiple options (the top is the default):
+# covstatis_results <- covstatis(cov_matrices, table_norm_type = "MFA", alpha_from_RV = TRUE)
+# covstatis_results <- covstatis(cov_matrices, table_norm_type = "MFA", alpha_from_RV = FALSE)
+# covstatis_results <- covstatis(cov_matrices, table_norm_type = "SS1", alpha_from_RV = TRUE)
+# covstatis_results <- covstatis(cov_matrices, table_norm_type = "SS1", alpha_from_RV = FALSE)
+# covstatis_results <- covstatis(cov_matrices, table_norm_type = "none", alpha_from_RV = TRUE)
+# covstatis_results <- covstatis(cov_matrices, table_norm_type = "none", alpha_from_RV = FALSE)
+
 
 ## and now a visual
 plot_covstatis(covstatis_results$compromise_component_scores, simplify2array(covstatis_results$partial_component_scores), display.fis.names = T)
