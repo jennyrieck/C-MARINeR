38dc646fb20aba0b827c42771442151354d0f6d8
Derek <derekbeaton@users.noreply.github.com>
2019-06-08 09:39:26 -0400

    inference working out a bit; bootstrap ok but split-half needs to be completed

--- code/covstatis/R/covstatis_inference.R
+++ code/covstatis/R/covstatis_inference.R
@@ -1,3 +1,7 @@
+
+
+#### I am missing something here...
+
 covstatis_inference <- function(cov_matrices, covstatis_results, iterations = 100){
 
   if(is.array(cov_matrices)){
@@ -16,7 +20,7 @@ covstatis_inference <- function(cov_matrices, covstatis_results, iterations = 10
 
   ## we need the matrices & fixed results
   # boot_compromise_component_scores <- vector(length(cov_matrices), "list")
-  boot_compromises <- boot_compromise_component_scores <- list()
+  splithalf_cors <- boot_compromise_component_scores_list <- list()
 
   for(i in 1:iterations){
 
@@ -37,31 +41,85 @@ covstatis_inference <- function(cov_matrices, covstatis_results, iterations = 10
 
     ## (1) make the alphas
     boot_cov_matrices %>%
-      normalize_matrices(., matrix_norm_type = covstatis_results$input_parameters$matrix_norm_type) %>%
+      normalize_matrices(., matrix_norm_type = covstatis_results$input_parameters$matrix_norm_type) ->
+      boot_cov_matrices
+
+
+    boot_cov_matrices %>%
       lapply(., c) %>%
       do.call(cbind, .) %>%
       compute_alphas(., alpha_from_RV = covstatis_results$input_parameters$alpha_from_RV) ->
       boot_alpha_weights
 
-
-    # cor_covstatis_results$compromise_matrix %*% cor_covstatis_results$compromise_decomposition_results$vectors %*% diag(1/sqrt(cor_covstatis_results$compromise_decomposition_results$values))
-    boot_compromises[[i]] <- make_compromise_matrix(boot_cov_matrices, boot_alpha_weights)
-
     ## (2 & 3) make compromise & project
     make_compromise_matrix(boot_cov_matrices, boot_alpha_weights) %*% (covstatis_results$compromise_decomposition_results$vectors %*% diag(1/sqrt(covstatis_results$compromise_decomposition_results$values))) ->
-      boot_compromise_component_scores[[i]]
+      boot_compromise_component_scores_list[[i]]
 
 
     # ## split half
-    # sh1_sample <- sample(1:length(cov_matrices), floor(length(cov_matrices)/2), replace = T)
-    # sh2_sample <- setdiff(1:length(cov_matrices), sh1_sample)
+    sh1_sample <- sample(1:length(cov_matrices), floor(length(cov_matrices)/2), replace = T)
+    sh2_sample <- setdiff(1:length(cov_matrices), sh1_sample)
 
+
+    cov_matrices[sh1_sample] %>%
+      double_center_matrices(.) ->
+      sh1_cov_matrices
+
+    cov_matrices[sh2_sample] %>%
+      double_center_matrices(.) ->
+      sh2_cov_matrices
+
+    if(covstatis_results$input_parameters$strictly_enforce_psd){
+      stopifnot(all(sapply(sh1_cov_matrices, is_sspsd_matrix, tol = tolerance)))
+    }
+    if(covstatis_results$input_parameters$strictly_enforce_psd){
+      stopifnot(all(sapply(sh2_cov_matrices, is_sspsd_matrix, tol = tolerance)))
+    }
+
+    ## (1) make the alphas
+    sh1_cov_matrices %>%
+      normalize_matrices(., matrix_norm_type = covstatis_results$input_parameters$matrix_norm_type) ->
+      sh1_cov_matrices
+
+    sh2_cov_matrices %>%
+      normalize_matrices(., matrix_norm_type = covstatis_results$input_parameters$matrix_norm_type) ->
+      sh2_cov_matrices
+
+    sh1_cov_matrices %>%
+      lapply(., c) %>%
+      do.call(cbind, .) %>%
+      compute_alphas(., alpha_from_RV = covstatis_results$input_parameters$alpha_from_RV) ->
+      sh1_alpha_weights
+
+    sh2_cov_matrices %>%
+      lapply(., c) %>%
+      do.call(cbind, .) %>%
+      compute_alphas(., alpha_from_RV = covstatis_results$input_parameters$alpha_from_RV) ->
+      sh2_alpha_weights
+
+    ## (2 & 3) make compromise & project
+    sh1_decomposition_results <- tolerance.eigen(make_compromise_matrix(sh1_cov_matrices, sh1_alpha_weights), symmetric = TRUE, tol = covstatis_results$input_parameters$tolerance)
+    sh2_decomposition_results <- tolerance.eigen(make_compromise_matrix(sh2_cov_matrices, sh2_alpha_weights), symmetric = TRUE, tol = covstatis_results$input_parameters$tolerance)
+
+    ## ok now project each split onto the results of the other...
+    # the correlate the split with the predict & add the abs(cor())
 
   }
 
+  ### this is ugly and needs substantial re-writing
+    ## probably also for memory and speed
+  boot.cube <- simplify2array(boot_compromise_component_scores_list)
+  boot.cube.mean <- apply(boot.cube, c(1, 2), mean)
+  boot.cube.mean_repeat <- array(boot.cube.mean, dim = c(dim(boot.cube)))
+  boot.cube.dev <- (boot.cube - boot.cube.mean_repeat)^2
+  s.boot <- sqrt(apply(boot.cube.dev, c(1, 2), mean))
+  boot.ratios <- boot.cube.mean/s.boot
+  boot.ratios[which(is.infinite(boot.ratios))] <- 0
+
   return(
     list(
-      covstatis_bootstrap_results = list(boot_compromise_component_scores = boot_compromise_component_scores, boot_compromises = boot_compromises),
+      covstatis_bootstrap_results = list(boot_compromise_component_scores_list = boot_compromise_component_scores_list,
+                                         boot.ratios = boot.ratios),
       covstatis_splithalf_results = list()
       )
     )
--- code/informal_tests/covstatis_inference_tests.R
+++ code/informal_tests/covstatis_inference_tests.R
@@ -11,12 +11,45 @@ load('/Data/TinyNKI/connectivity_cubes.rda')
 
 cor_covstatis_results <- covstatis(conn.cube.tog, strictly_enforce_psd = FALSE)
 
-  ### this is way slower than it should be...
+# 
+# conn.cube.tog <- covstatis::array2list(conn.cube.tog)
+# 
+# # cor_covstatis_results
+# ### test it here...
+# bootstrap_sample <- sample(1:length(conn.cube.tog), replace = T)
+# 
+# conn.cube.tog[bootstrap_sample] %>%
+#   double_center_matrices(.) ->
+#   boot_cov_matrices
+# 
+# 
+# ## (1) make the alphas
+# boot_cov_matrices %>%
+#   normalize_matrices(., matrix_norm_type = cor_covstatis_results$input_parameters$matrix_norm_type) ->
+#   boot_cov_matrices
+# 
+# boot_cov_matrices %>%
+#   lapply(., c) %>%
+#   do.call(cbind, .) %>%
+#   compute_alphas(., alpha_from_RV = cor_covstatis_results$input_parameters$alpha_from_RV) ->
+#   boot_alpha_weights
+# 
+# 
+# boot_compromise <- make_compromise_matrix(boot_cov_matrices, boot_alpha_weights)
+# 
+# ## (2 & 3) make compromise & project
+# make_compromise_matrix(boot_cov_matrices, boot_alpha_weights) %*% (cor_covstatis_results$compromise_decomposition_results$vectors %*% diag(1/sqrt(cor_covstatis_results$compromise_decomposition_results$values))) ->
+#   boot_compromise_component_scores
+# 
+# 
+# 
+# ### this is way slower than it should be...
 covstatis_inference_results <- covstatis_inference(conn.cube.tog, cor_covstatis_results)
+# 
+# 
+# apply(simplify2array(covstatis_inference_results$covstatis_bootstrap_results$boot_compromise_component_scores),c(1,2), mean)
+
+plot(cor_covstatis_results$compromise_component_scores)
+points(apply(simplify2array(covstatis_inference_results$covstatis_bootstrap_results$boot_compromise_component_scores),c(1,2), mean), col="red", pch=20)
 
 
-
-apply(simplify2array(covstatis_inference_results$covstatis_bootstrap_results$boot_compromise_component_scores),c(1,2), mean)
-
-# plot(cor_covstatis_results$compromise_component_scores)
-# points(apply(simplify2array(covstatis_inference_results$covstatis_bootstrap_results$boot_compromise_component_scores),c(1,2), mean), col="red", pch=20)
