060aac96e7e7b9f2059ee6cefa697dca636d80be
Derek <derekbeaton@users.noreply.github.com>
2019-07-03 14:03:38 -0400

    a commit to just save progress, but the supplementary projections will be re-done

--- code/covstatis/R/covstatis_inference.R
+++ code/covstatis/R/covstatis_inference.R
@@ -61,59 +61,43 @@ covstatis_inference <- function(cov_matrices, covstatis_results, iterations = 10
     sh2_sample <- setdiff(1:length(cov_matrices), sh1_sample)
 
 
-    cov_matrices[sh1_sample] %>%
-      double_center_matrices(.) ->
-      sh1_cov_matrices
+    sh1_covstatis <- covstatis(cov_matrices[sh1_sample],
+                               alpha_from_RV = covstatis_results$input_parameters$alpha_from_RV,
+                               matrix_norm_type = covstatis_results$input_parameters$matrix_norm_type,
+                               tolerance = covstatis_results$input_parameters$tolerance,
+                               strictly_enforce_psd = covstatis_results$input_parameters$strictly_enforce_psd
+                              )
 
-    cov_matrices[sh2_sample] %>%
-      double_center_matrices(.) ->
-      sh2_cov_matrices
+    sh2_covstatis <- covstatis(cov_matrices[sh2_sample],
+                               alpha_from_RV = covstatis_results$input_parameters$alpha_from_RV,
+                               matrix_norm_type = covstatis_results$input_parameters$matrix_norm_type,
+                               tolerance = covstatis_results$input_parameters$tolerance,
+                               strictly_enforce_psd = covstatis_results$input_parameters$strictly_enforce_psd
+    )
 
-    if(covstatis_results$input_parameters$strictly_enforce_psd){
-      stopifnot(all(sapply(sh1_cov_matrices, is_sspsd_matrix, tol = tolerance)))
-    }
-    if(covstatis_results$input_parameters$strictly_enforce_psd){
-      stopifnot(all(sapply(sh2_cov_matrices, is_sspsd_matrix, tol = tolerance)))
-    }
 
-    ## (1) make the alphas
-    sh1_cov_matrices %>%
-      normalize_matrices(., matrix_norm_type = covstatis_results$input_parameters$matrix_norm_type) ->
-      sh1_cov_matrices
+    # make_compromise_matrix(boot_cov_matrices, boot_alpha_weights) %*% (covstatis_results$compromise_decomposition_results$vectors %*% diag(1/sqrt(covstatis_results$compromise_decomposition_results$values))) ->
+    #   boot_compromise_component_scores_list[[i]]
 
-    sh2_cov_matrices %>%
-      normalize_matrices(., matrix_norm_type = covstatis_results$input_parameters$matrix_norm_type) ->
-      sh2_cov_matrices
 
-    sh1_cov_matrices %>%
-      lapply(., c) %>%
-      do.call(cbind, .) %>%
-      compute_alphas(., alpha_from_RV = covstatis_results$input_parameters$alpha_from_RV) ->
-      sh1_alpha_weights
 
-    sh2_cov_matrices %>%
-      lapply(., c) %>%
-      do.call(cbind, .) %>%
-      compute_alphas(., alpha_from_RV = covstatis_results$input_parameters$alpha_from_RV) ->
-      sh2_alpha_weights
-
-    ## (2 & 3) make compromise & project
-    sh1_decomposition_results <- tolerance.eigen(make_compromise_matrix(sh1_cov_matrices, sh1_alpha_weights), symmetric = TRUE, tol = covstatis_results$input_parameters$tolerance)
-    sh2_decomposition_results <- tolerance.eigen(make_compromise_matrix(sh2_cov_matrices, sh2_alpha_weights), symmetric = TRUE, tol = covstatis_results$input_parameters$tolerance)
 
     ## ok now project each split onto the results of the other...
     # the correlate the split with the predict & add the abs(cor())
 
+
+
   }
 
-  ### this is ugly and needs substantial re-writing
-    ## probably also for memory and speed
-  boot.cube <- simplify2array(boot_compromise_component_scores_list)
-  boot.cube.mean <- apply(boot.cube, c(1, 2), mean)
-  boot.cube.mean_repeat <- array(boot.cube.mean, dim = c(dim(boot.cube)))
-  boot.cube.dev <- (boot.cube - boot.cube.mean_repeat)^2
-  s.boot <- sqrt(apply(boot.cube.dev, c(1, 2), mean))
-  boot.ratios <- boot.cube.mean/s.boot
+  ## this could benefit from less ugliness and more efficiency.
+  ## probably also for memory and speed
+  boot.cube.mean <- apply(simplify2array(boot_compromise_component_scores_list), c(1, 2), mean)
+  boot.cube.dev <- sapply(boot_compromise_component_scores_list,
+                          function(this_slice, mean_slice){(this_slice - mean_slice)^2},
+                          mean_slice = boot.cube.mean,
+                          simplify = FALSE,
+                          USE.NAMES = TRUE)
+  boot.ratios <- boot.cube.mean / (sqrt(apply(simplify2array(boot.cube.dev), c(1, 2), mean)))
   boot.ratios[which(is.infinite(boot.ratios))] <- 0
 
   return(
